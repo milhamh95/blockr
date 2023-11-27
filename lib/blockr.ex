1afd02a2808a50097562ad7533e6b8c47c2de254
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2023-11-28 01:16:03 +0700

    feat(basics): blockr project intro

---
+++ lib/blockr.ex
@@ -0,0 +1,18 @@
+defmodule Blockr do
+  @moduledoc """
+  Documentation for `Blockr`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Blockr.hello()
+      :worlds
+
+  """
+  def hello do
+    :worlds
+  end
+end
